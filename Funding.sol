299591c414d139f80ee29606301aa59ab60acd8e
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-09-03 06:13:36 -0400

    Create Funding.sol

---
+++ Funding.sol
@@ -0,0 +1,16 @@
+// SPDX -License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract Funding {
+    address payable public owner;
+    uint256 public finishesAt;
+    uint public goal;
+    uint256 public moneyRaised;
+    mapping(address => uint256) public balances;
+
+    constructor(uint256 _duration, uint _goal) {
+        owner = payable(msg.sender);
+        finishesAt = block.timestamp + _duration;
+        goal = _goal;
+    }
