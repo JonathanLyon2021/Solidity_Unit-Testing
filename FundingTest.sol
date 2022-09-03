9052061ababdc85b55d7c43d95ad2e278f4a2726
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-09-03 06:20:52 -0400

    Added the last 3 final methods/functions

--- FundingTest.sol
+++ FundingTest.sol
@@ -49,4 +49,25 @@ contract FundingTest {
         funding.donate{value: 15000000 gwei}();
         Assert.equal(funding.balances(address(this)), 20000000 gwei, 'Donator balance is different from donations');
     }
+    
+    function testAcceptingDonations() public {
+       // Funding funding = new Funding();
+        Assert.equal(funding.moneyRaised(), 0, 'Initial raised amount is different from 0');
+
+        funding.donate{value: 10000000 gwei}();
+        funding.donate{value: 20000000 gwei}();
+        Assert.equal(funding.moneyRaised(), 30000000 gwei, 'Raised amount is different from sum of donations');
+
+    }
+    function testSettingAnOwnerDuringCreation() public {
+      // Funding funding = new Funding();
+        Assert.equal(funding.owner(), address(this), 'The owner is different from the deployer');
+    }
+
+    function testSettingAnOwnerOfDeployedContract() public {
+        Funding newFund = Funding(DeployedAddresses.Funding());
+        Assert.equal(newFund.owner(), msg.sender, 'The owner is different from the deployer');
+    }
+}
+    
 
