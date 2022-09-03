f6d43754aa30134dd1cb0d8333e3cf8f7037ccb3
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-09-03 06:31:20 -0400

    Added 3 test cases

--- FundingTest.js
+++ FundingTest.js
@@ -38,3 +38,40 @@ contract("Funding", async accounts => {
             assert.ok(/revert/.test(err.message));
     }
 });
+    
+    it("allows to withdraw funds after time is up and goal is not reached", async () => {
+        await funding.donate({ from: secondAccount, value: 50000000 * GWEI });
+
+        const initBalance = await web3.eth.getBalance(secondAccount);
+        assert.equal(await funding.balances.call(secondAccount), 50000000 * GWEI);
+
+        await utils.timeTravel(web3, DAY);
+        await funding.refund({ from: secondAccount });
+        const finalBalance = await web3.eth.getBalance(secondAccount);
+        assert.isTrue(parseInt(finalBalance, 10) > parseInt(initBalance, 10));
+    });
+
+    it("does not allow to withdraw funds after time is up and goal is reached", async () => {
+        await funding.donate({ from: secondAccount, value: 100000000 * GWEI  });
+        assert.equal(await funding.balances.call(secondAccount), 100000000 * GWEI);
+
+        await utils.timeTravel(web3, DAY);
+        try {
+            await funding.refund({ from: secondAccount });
+            assert.fail();
+        } catch (err) {
+            assert.ok(/revert/.test(err.message));
+        }
+    });
+    
+     it("does not allow owner to withdraw funds defore time is up and goal is not reached", async () => {
+        await funding.donate({ from: secondAccount, value: 50000000 * GWEI });
+        assert.equal(await funding.balances.call(secondAccount), 50000000 * GWEI);
+        try {
+            await funding.refund({ from: secondAccount });
+            assert.fail();
+        } catch (err) {
+            assert.ok(/revert/.test(err.message));
+        }
+    });
+
