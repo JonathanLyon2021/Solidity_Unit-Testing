d3e66b07e0fbd690e015066e96e458b9500f3ec9
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-09-03 06:19:47 -0400

    Added 3 public methods/functions

--- FundingTest.sol
+++ FundingTest.sol
@@ -15,3 +15,38 @@ contract FundingTest {
     function beforeEach() public {
         funding = new Funding(1 days, 100000000 gwei);
     }
+    
+     function testWithdrawalByOwner() public {
+        uint initBalance = address(this).balance;
+        funding.donate{value: 50000000 gwei}();
+
+        bytes memory bs = abi.encodePacked((keccak256('withdraw()')));
+        (bool result,) = address(funding).call(bs);
+        Assert.equal(result, false, 'Allows for withdrawal before reaching the goal');
+
+        funding.donate{value: 50000000 gwei}();
+        Assert.equal(address(this).balance, initBalance - 100000000 gwei,
+        "Balance before withdrawal doesn't correspond to sum of donations");
+
+        bs = abi.encodePacked((keccak256('withdraw()')));
+        (result,) = address(funding).call(bs);Assert.equal(result, true, 'Does not allow for withdrawal after reaching the goal');
+        Assert.equal(address(this).balance, initBalance,
+        "Balance after withdrawal doesn't correspond to sum of donations");
+
+    }
+    
+    function testDonatingAfterTimeIsUp() public {
+        Funding newFund = new Funding(0, 100000000 gwei);
+        bytes memory bs = abi.encodePacked((keccak256('donate()')));
+
+        (bool result,) = address(newFund).call{value: 100000000 gwei}(bs);
+        Assert.equal(result, false, 'Allows for donations when time is up');
+    }
+
+    function testTrackinigDonatorsBalance() public {
+       // Funding funding = new Funding();
+        funding.donate{value: 5000000 gwei}();
+        funding.donate{value: 15000000 gwei}();
+        Assert.equal(funding.balances(address(this)), 20000000 gwei, 'Donator balance is different from donations');
+    }
+
